d292fcdeae9de4a74bd39ce9dbf53a4f97a10f00
ianK-code <kian99564@gmail.com>
2023-03-23 20:54:06 +0300

    Setting things up

---
+++ db/seeds.rb
@@ -0,0 +1,26 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+# 10.times do
+
+#     Hero.create(
+#         name: Faker::Name.unique.name
+#         super_name: Faker::Superhero.name
+#     )
+
+#     Power.create(
+#         name: Faker::Superhero.power
+#         description: Faker::Lorem.paragraph(sentence_count: 3)
+#     )
+
+#     HeroPower.create(
+#         strength: Faker::Superhero.descriptor
+#         hero_id: Hero.all.sample.id
+#         power_id: Power.all.sample.id
+#     )
+# end
