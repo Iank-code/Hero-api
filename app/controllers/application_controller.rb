4a914d91183304efc028b6706cace3fa1a24fbf7
ianK-code <kian99564@gmail.com>
2023-03-24 19:48:23 +0300

    Started error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+    rescue_from StandardError, with: :standard_error
+
+    def app_response(message: 'success', status: 200, data: nil)
+        render json: {
+            message: message,
+            data: data
+        }, status: status
+    end
+
+    def standard_error(exception)
+        app_response(message: 'failed', data: { info: exception.message }, status: :unprocessable_entity)
+    end
 end
